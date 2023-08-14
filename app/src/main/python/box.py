a2e50c5a933092e0503f1c74dda1bf6d796b310e
huni00807 <huni00807@gmail.com>
2023-08-15 01:36:18 +0900

    feat:원본 및 crop 비율 조정, 그에따른 xyxy조정

--- app/src/main/python/box.py
+++ app/src/main/python/box.py
@@ -27,7 +27,29 @@ def rotate_image_with_exif(image):
         pass  # Exif 정보가 없는 경우
 
     return image
-
+#받은 이미지를 640*640비율에 맞게 자르는 함수
+def resize_image(original : cv2, crop : cv2, xyxy):
+    #3024*4032가정하고 640*853으로 비율 맞춰서 자르기
+    original_resize = cv2.resize(original, (640, 853))
+    #자른 후 위 아래 100씩 자르기
+    original_resize = original_resize[103:743, 0:640]
+    print("original xy : ", original.shape[1], original.shape[0])
+    print("original_resize xy : ", original_resize.shape[1], original_resize.shape[0])
+    print("xyxy", xyxy)
+    #비율
+    original_ratio = 640 / original.shape[1]
+    xyxy_ratio = original.shape[1] / 640
+    #crop된 이미지 비율 맞춰서 resize
+    crop_resize = cv2.resize(crop, dsize=(0,0), fx=original_ratio, fy=original_ratio) 
+    print("crop xy : ", crop.shape[1], crop.shape[0])
+    print("crop_resize xy : ", crop_resize.shape[1], crop_resize.shape[0])
+    #비율에 맞게 xyxy조정
+    new_xyxy = [float(xyxy[0] / xyxy_ratio),
+                float(xyxy[1] / xyxy_ratio - 100),
+                float(xyxy[2] / xyxy_ratio),
+                float(xyxy[3] / xyxy_ratio - 100)]
+    print(new_xyxy)
+    return original_resize, crop_resize, new_xyxy
 
 def calculate_box_size(original : Image, crop : Image, params, xyxy, show=False):
 
@@ -37,6 +59,7 @@ def calculate_box_size(original : Image, crop : Image, params, xyxy, show=False)
     crop = np.array(crop)   # image를 cv2에서 사용 가능하게 변환
     crop = cv2.cvtColor(crop, cv2.COLOR_RGB2BGR)
 
+    original, crop, xyxy = resize_image(original, crop, xyxy)
     # box, xyxy = detector.detect(image) #박스 감지하고 crop 리턴
 
     crop, original_ratio = simplifier.simplify(crop) #배경 지우고, 외곽선 검출
@@ -102,8 +125,8 @@ def main(original_image_data, crop_image_data, params, xyxy):
     return result
 
 if __name__ == '__main__':
-    image = Image.open("modules/images_cali/bbox.jpg") # 이미지 테스트
-    image_cali = Image.open("modules/images_cali/check3.jpg") #calibration
+    image = Image.open("images_cali/bbox.jpg") # 이미지 테스트
+    image_cali = Image.open("images_cali/check3.jpg") #calibration
     image = rotate_image_with_exif(image)
     image_cali = rotate_image_with_exif(image_cali)
     params = calculate_camera_parameters(image_cali)
